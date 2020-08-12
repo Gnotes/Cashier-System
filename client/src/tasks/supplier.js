ee67cc6589c9d51ce5acf894fb5576e16f2a3b74
xing.he <1245036094@qq.com>
2020-08-12 20:32:36 +0800

    feat: add supplier's back phone

--- client/src/tasks/supplier.js
+++ client/src/tasks/supplier.js
@@ -1,31 +1,34 @@
 export class SupplierTask {
-    static createSupplier(ajax, name, phone, description) {
-        const data = {
-            name
-        };
+  static createSupplier(ajax, name, phone, phone2, description) {
+    const data = {
+      name,
+    };
 
-        if (phone) {
-            data.phone = phone
-        }
-        if (description) {
-            data.description = description;
-        }
-
-        return ajax.post("/api/warehouse/suppliers/create", data);
+    if (phone) {
+      data.phone = phone;
+    }
+    if (phone2) {
+      data.phone2 = phone2;
+    }
+    if (description) {
+      data.description = description;
     }
 
-    static getSupplier(ajax) {
-        return ajax.get("/api/warehouse/suppliers");
-    }
+    return ajax.post("/api/warehouse/suppliers/create", data);
+  }
 
-    static updateSupplier(ajax, name, update_value) {
-        return ajax.put("/api/warehouse/suppliers/update", {
-            name,
-            update_value
-        });
-    }
+  static getSupplier(ajax) {
+    return ajax.get("/api/warehouse/suppliers");
+  }
 
-    static deleteSupplier(ajax, name) {
-        return ajax.delete(`/api/warehouse/suppliers/delete/${encodeURIComponent(name)}`);
-    }
+  static updateSupplier(ajax, name, update_value) {
+    return ajax.put("/api/warehouse/suppliers/update", {
+      name,
+      update_value,
+    });
+  }
+
+  static deleteSupplier(ajax, name) {
+    return ajax.delete(`/api/warehouse/suppliers/delete/${encodeURIComponent(name)}`);
+  }
 }
